dddd536a5b6d823a69e67f78b482c739eabec610
Vedant Jha <122289414+vedantjha06@users.noreply.github.com>
2023-09-27 12:01:50 +0530

    Add files via upload

---
+++ GLCA-Assignment1/src/com/gradedproject/q1/AdminDepartment.java
@@ -0,0 +1,15 @@
+package com.gradedproject.q1;
+
+class AdminDepartment extends SuperDepartment {
+	String departmentName() {
+		return "admin department";
+	}
+
+	String getTodaysWork() {
+		return "Complete your document submission";
+	}
+
+	String getWorkDeadline() {
+		return "Complete it by EOD";
+	}
+}
---
+++ GLCA-Assignment1/src/com/gradedproject/q1/TechDepartment.java
@@ -0,0 +1,21 @@
+package com.gradedproject.q1;
+
+class TechDepartment extends SuperDepartment {
+
+	String departmentName() {
+		return "tech department";
+	}
+
+	String getTodaysWork() {
+		return "Complete coding of Module 1";
+	}
+
+	String getWorkDeadline() {
+		return "Complete it by EOD";
+	}
+
+	String getTechStackInformation() {
+		return "Core Java";
+	}
+
+}
